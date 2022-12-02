e9128f181b45dd070ed1521705df7b0ea4b0dc31
SheilaNS <shei.nsantos@gmail.com>
2022-12-02 13:20:14 -0300

    feat: process done

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
-def process(path_file, instance):
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
     """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    lines = txt_importer(path_file)
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+    instance.enqueue(file_info)
+    print(file_info)
 
 
 def remove(instance):
