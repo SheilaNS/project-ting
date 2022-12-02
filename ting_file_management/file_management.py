1a0c207f3cbf4f75587dabea8e34ac2cf2bf3ad2
SheilaNS <shei.nsantos@gmail.com>
2022-12-02 12:47:55 -0300

    feat: txt_importer success done

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -5,3 +5,5 @@ def txt_importer(path_file):
     """Aqui irá sua implementação"""
     if not path_file.endswith(".txt"):
         sys.stderr.write("Formato inválido\n")
+    with open(path_file, mode="r", encoding="utf-8") as file:
+        return file.read().split("\n")
