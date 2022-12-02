2edc71eef5701d24d53f2d7fc7c818b433ba80b0
SheilaNS <shei.nsantos@gmail.com>
2022-12-02 12:51:50 -0300

    feat: txt_importer don't exists done

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -5,5 +5,8 @@ def txt_importer(path_file):
     """Aqui irá sua implementação"""
     if not path_file.endswith(".txt"):
         sys.stderr.write("Formato inválido\n")
-    with open(path_file, mode="r", encoding="utf-8") as file:
-        return file.read().split("\n")
+    try:
+        with open(path_file, mode="r", encoding="utf-8") as file:
+            return file.read().split("\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
