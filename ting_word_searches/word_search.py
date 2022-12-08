020e1fd9f815ef54506ab3ab5c26cbbbe1a384f7
SheilaNS <shei.nsantos@gmail.com>
2022-12-08 13:50:53 -0300

    feat: exists_word done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,30 @@
-def exists_word(word, instance):
+from ting_file_management.queue import Queue
+
+
+def get_line(index, content, is_content):
+    if is_content:
+        return {"linha": index, "conteudo": content}
+    return {"linha": index}
+
+
+def exists_word(word, instance: Queue, is_content=False):
     """Aqui irá sua implementação"""
+    result = []
+    for info in range(len(instance)):
+        data = instance.search(info)
+        occurrences = [
+            get_line(index + 1, content, is_content)
+            for index, content in enumerate(data["linhas_do_arquivo"])
+            if word.lower() in content.lower()
+        ]
+        result_item = {
+            "palavra": word,
+            "arquivo": data["nome_do_arquivo"],
+            "ocorrencias": occurrences,
+        }
+        if occurrences:
+            result.append(result_item)
+    return result
 
 
 def search_by_word(word, instance):
